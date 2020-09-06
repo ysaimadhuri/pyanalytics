21ca798656ac653111bfb98e31d87a48b83e6fd0
ysaimadhuri <madhuri.yenamandra@gmail.com>
2020-09-06 13:57:14 +0530

    Create untitled1.py
    Session 1 Sep 5th

---
+++ untitled1.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 20:12:14 2020
+
+@author: madhu
+"""
+
+print?
+print("Madhuri")
+print("Madhuri", "Sai", sep='***',end="    ")
+print("Madhuri","Sai",sep='****')
+
+help(print)
+
+help("OPERAT
+     
+input("Your country")
+
+Number=input()
+print(Number)
+type(Number)
+
+#type casting or conversion
+
+Num=int(Number)
+type(Num)
+
+country="India"
+print("I live in", country, "which is very good country)
+print("I live in {0}".format(country))
+
+countrycode=91
+print("I live in", countrycode,  "which is very good country")
+print("I live in", {0},  "which is very good country")
+
+a=1.11
+b=int(a)
+c=float(b)
+print(b)
+
+x=2
+y=3
+z=x+y
+print(z)
+
+print(x+y)
+print(x-y)
+x--
+x-=1
+print(x)
+
